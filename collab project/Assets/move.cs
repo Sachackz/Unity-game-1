2be66c3f536eef909171734bff232a98038fc0bd
Sachackz <61701592+Sachackz@users.noreply.github.com>
2022-04-20 00:47:19 +0100

    Update move.cs

--- collab project/Assets/move.cs
+++ collab project/Assets/move.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    Public playercontroller pc;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float X = Input.getaxis("Horizontal");
+        float z = Input.getaxis("Vertical");
+        Vector3 move = vector3.forward * X + vector3.right * z;
+        pc.Move(move * speed * time.deltatime());
     }
 }
